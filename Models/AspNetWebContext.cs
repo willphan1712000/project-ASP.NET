4a7d020c0433350ac5be2e703373f3b4df8d360a
Thanh Nha (Will) Phan <phanthanhnha1712000@gmail.com>
2026-02-24 19:02:49 -0500

    Update .gitignore, add env variable

--- Models/AspNetWebContext.cs
+++ Models/AspNetWebContext.cs
@@ -22,7 +22,7 @@ public partial class AspNetWebContext : IdentityDbContext<User>
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseMySql("server=localhost;database=ASP_NET_Web;user=root", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.4.28-mariadb"));
+        => optionsBuilder.UseMySql(Environment.GetEnvironmentVariable("DATABASE_SOURCE"), Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.4.28-mariadb"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
